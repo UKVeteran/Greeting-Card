9975bcadc04cec8689d47d3bb4782d208aa5602b
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-10-16 12:22:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,20 +14,20 @@ setInterval(() => {
 }, 500);
 
 if (curHr < 12) {
-    document.getElementById("greet").innerHTML = "Good Morning!! ";
+    document.getElementById("greet").innerHTML = "Good Morning!!";
     document.getElementById("greet-img").src = "https://img.icons8.com/color/48/000000/sun--v3.png"
-    document.getElementById("message").innerHTML = "Have A Great Day!";
+    document.getElementById("message").innerHTML = "Have a good day!";
     document.getElementById("lottie").src = "https://assets7.lottiefiles.com/packages/lf20_b8z1hjre.json";
 }
 else if (curHr < 16) {
     document.getElementById("greet").innerHTML = "Good Afternoon!!";
     document.getElementById("greet-img").src = "https://img.icons8.com/office/48/000000/fog-day--v2.png";
-    document.getElementById("message").innerHTML = "Enjoy The Rest of Your Day!";
+    document.getElementById("message").innerHTML = "Have a good rest!";
     document.getElementById("lottie").src = "https://assets6.lottiefiles.com/private_files/lf30_1xg5wkc5.json";
 }
 else {
     document.getElementById("greet").innerHTML = "Good Evening!!";
     document.getElementById("greet-img").src = "https://img.icons8.com/office/50/000000/partly-cloudy-night--v2.png";
-    document.getElementById("message").innerHTML = "Make The Most of Your Evening!";
+    document.getElementById("message").innerHTML = "Have good snacks!";
     document.getElementById("lottie").src = "https://assets5.lottiefiles.com/packages/lf20_rlmeimrj.json";
 }
